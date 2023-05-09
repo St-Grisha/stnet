7e83a992926cdfd5de5407a2845122e8967d54b2
St-Grisha <gastepanov@edu.hse.ru>
2023-05-09 11:26:08 +0300

    выбор токенов

--- jediswap.py
+++ jediswap.py
@@ -4,7 +4,7 @@ import subprocess
 import argparse
 
 
-def main(eth_amount, my_address):
+def main(eth_amount, my_address, token_from='ETH', token_to='USDT'):
     contract_address = '0x05900cfa2b50d53b097cb305d54e249e31f24f881885aae5639b0cd6af4ed298'
     # my_address = '0x054e3682d5227b7f0c72cfcba78c77e133909180faba7f2deb581547ab2eca76'
 
@@ -16,17 +16,30 @@ def main(eth_amount, my_address):
 
     # команда ниже разрешает использование эфира 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
     # контракту 0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023 -- это JediSwap
-    # команда тратит газ     
-    command_approve = f'starknet invoke --address 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7 --function approve --abi approve.json --inputs 0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023 {gwei_amount} 0' 
+    # команда тратит газ   
+    
+    # ETH_ADDRESS = ''
+    address_book = dict()
+    address_book["ETH"] = '0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
+    
+    address_book["USDT"] = '0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8'
+    address_book["USDC"] = '0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8'
+    address_book["DAI"] = '0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3'
+    address_book["WBTC"] = '0x03fe2b97c1fd336e750087d68b9b867997fd64a2661ff3ca5a7c771641e8e7ac'
+    
+    address_from = address_book[token_from]
+    address_to = address_book[token_to]
+    command_approve = f'starknet invoke --address {address_from} --function approve --abi approve.json --inputs 0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023 {gwei_amount} 0' 
 
 
-#     p = subprocess.Popen(command_approve, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(command_approve, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-#     for i in p.stdout.readlines():
-#         print(i)
+    for i in p.stdout.readlines():
+        print(i)
 
     
-    get_out_amount = f'starknet call --address 0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023 --function get_amounts_out --abi jediswap.json --inputs {gwei_amount} 0 2 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7 0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8'
+    get_out_amount = f'starknet call --address 0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023 --function get_amounts_out --abi jediswap.json --inputs {gwei_amount} 0 2 {address_from} {curr_address}'
+    time.sleep(60*10)
     
     answer = b'trying'
     while answer == b'trying':
@@ -38,7 +51,7 @@ def main(eth_amount, my_address):
         print('we here')
     # if answer != b'trying':
     
-    out_amount = int(int(answer) * 0.98)
+    out_amount = int(int(answer) * 0.98) # Допустимое проскальзывание 2%
     
     
     # time.sleep(60*10)
@@ -48,7 +61,8 @@ def main(eth_amount, my_address):
     # теперь непосредственно свапаем разрешенное количество эфира 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
     # на USDT 0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8 
     # команда тратит газ
-    jediswap_command = f"starknet invoke --address 0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023 --function swap_exact_tokens_for_tokens --abi jediswap.json --inputs {gwei_amount} 0 {out_amount} 0 2 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7 0x68f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8 {my_address} {end_time}" 
+    
+    jediswap_command = f"starknet invoke --address 0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023 --function swap_exact_tokens_for_tokens --abi jediswap.json --inputs {gwei_amount} 0 {out_amount} 0 2 {address_from} {curr_address} {my_address} {end_time}" 
     
     p = subprocess.Popen(jediswap_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for i in p.stdout.readlines():
@@ -57,6 +71,12 @@ def main(eth_amount, my_address):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Choose address and amount')
     parser.add_argument('eth_amount', type=float, help='Choose amount of ETH to swap in USDT')
-    parser.add_argument('my_address', type=float, help='Choose address')
+    parser.add_argument('my_address', type=str, help='Choose address')
+    parser.add_argument('token_from', type=str, help='Choose token')
+    parser.add_argument('token_to', type=str, help='Choose token')
+    
     args = parser.parse_args()
-    main(args.eth_amount, args.my_address)
+    if ['ETH'] in [args.token_from, args.token_to]:
+        main(args.eth_amount, args.my_address, args.token_from, args.token_to)
+    else:
+        print('one of the token must be an ETH')
